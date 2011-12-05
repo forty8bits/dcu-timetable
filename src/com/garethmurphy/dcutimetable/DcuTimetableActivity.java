69adf1c83c7dbca4b917a9363f8ec8c5fc9303c3
Gareth Murphy <gareth.cpm@gmail.com>
2011-12-05 01:13:28 +0000

    Eclipse GitHub setup test.

--- src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
+++ src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
@@ -3,6 +3,7 @@ package com.garethmurphy.dcutimetable;
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class DcuTimetableActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
