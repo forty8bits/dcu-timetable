d4a8d287178e1a3008396071368cc110516b0051
Gareth Murphy <gareth.cpm@gmail.com>
2011-12-06 01:37:00 +0000

    Created basic functionality.

--- gen/com/garethmurphy/dcutimetable/R.java
+++ gen/com/garethmurphy/dcutimetable/R.java
@@ -11,7 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int dculogo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int button1=0x7f050006;
+        public static final int editText1=0x7f050002;
+        public static final int imageView1=0x7f050000;
+        public static final int radio0=0x7f050004;
+        public static final int radio1=0x7f050005;
+        public static final int radioGroup1=0x7f050003;
+        public static final int textView1=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
+++ src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
@@ -1,7 +1,14 @@
 package com.garethmurphy.dcutimetable;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 
 public class DcuTimetableActivity extends Activity {
@@ -10,5 +17,31 @@ public class DcuTimetableActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final EditText classCode = (EditText)findViewById(R.id.editText1);
+        final RadioButton sem1 = (RadioButton)findViewById(R.id.radio0);
+        final Button btn = (Button)findViewById(R.id.button1);
+        
+        btn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				if (classCode.length() == 0) return;
+				
+				String url = "http://www.dcu.ie/timetables/feed.php3?per=1&hour=1-28&template=student";
+				
+				// Add the class code to the URL.
+				url += "&prog=" + classCode.getText();
+				
+				if (sem1.isChecked()) {
+					url += "&week1=1&week2=12";
+				} else {
+					url += "&week1=13&week2=24";
+				}
+				
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(url));
+				startActivity(i);
+			}
+		});
     }
 }
