da55f861df895be5e3d68ad0b4a63a857828fdba
Gareth Murphy <gareth.cpm@gmail.com>
2011-12-07 17:07:33 +0000

    Cleaned up Activity code and fixed RadioGroup regression which allowed none or both RadioButtons to be selected.

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # DCU Timetable
 
-A simple Android application for students of Dublin City University (DCU) to quickly retrieve their online timetables.
+A simple Android application for students of Dublin City University (DCU) to
+quickly retrieve their online timetables on the move, without the hassle of
+having to go through the various cumbersome methods of finding it (either
+through the DCU website or Moodle).
 
-Benefits include:
+Although right now this app is fairly basic, and only allows you to look at the
+online version through your phones built-in browser, future improvements will
+include:
 
-- Don't have to go through slow Moodle process of getting the timetable.
-- Future versions will have new features such as local storage of users timetable and better display.
++ Storage of your timetable locally, allowing access when no network is
+  available and also saving battery.
++ Parsing of the timetable data for much improved display on your mobile device.
+
+Any suggestions for features are welcome!
--- src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
+++ src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
@@ -11,36 +11,40 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class DcuTimetableActivity extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final EditText classCode = (EditText)findViewById(R.id.editText1);
-        final EditText classYear = (EditText)findViewById(R.id.editText2);
-        final RadioButton sem = (RadioButton)findViewById(R.id.radioButton1);
-        final Button btn = (Button)findViewById(R.id.button1);
-        
+        // Setup the event handler to process details and send the Intent.
+        Button btn = (Button)findViewById(R.id.button1);
         btn.setOnClickListener(new OnClickListener() {
+        	
+        	private EditText code = (EditText)findViewById(R.id.cdEntry);   
+			private EditText year = (EditText)findViewById(R.id.yrEntry);   
+			private RadioButton sem1 = (RadioButton)findViewById(R.id.sem1Btn);
 			
 			public void onClick(View v) {
-				if (classCode.length() == 0 || classYear.length() == 0) return;
+				if (code.length() == 0 || year.length() == 0) return;
 				
-				String url = "http://www.dcu.ie/timetables/feed.php3?hour=1-28&template=student";
+				String url = "http://www.dcu.ie/timetables/feed.php3" 
+						   + "?hour=1-28&template=student";
 				
 				// Add the class code to the URL.
-				url += "&prog=" + classCode.getText();
+				url += "&prog=" + code.getText();
 				
 				// The year of study.
-				url += "&per=" + classYear.getText();
+				url += "&per=" + year.getText();
 				
-				if (sem.isChecked()) {
+				if (sem1.isChecked()) {
 					url += "&week1=1&week2=12";
 				} else {
 					url += "&week1=13&week2=24";
 				}
 				
+				// Send out the intent to open the timetable in default browser.
 				Intent i = new Intent(Intent.ACTION_VIEW);
 				i.setData(Uri.parse(url));
 				startActivity(i);
