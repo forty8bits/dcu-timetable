15acb0387b4065f6777010f602b7f1569fe5f2f6
Colm Moore <mooreco4@gmail.com>
2011-12-08 14:40:22 +0000

    Changed the main.xml to a new homescreen layout and added the two new activities to AndroidManifest.xml

--- src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
+++ src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
@@ -2,15 +2,10 @@ package com.garethmurphy.dcutimetable;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
 
 public class DcuTimetableActivity extends Activity {
 	
@@ -21,64 +16,21 @@ public class DcuTimetableActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        // Setup the event handler to process details and send the Intent.
-        final Button btn = (Button)findViewById(R.id.button1);
-        final EditText classCode = (EditText)findViewById(R.id.cdEntry);
-        
-       // This lump of code disables the keyboard from popping up on launch 
-        
-        classCode.setInputType(InputType.TYPE_NULL);
-    
-        classCode.setOnTouchListener(new View.OnTouchListener() {
-            
-            public boolean onTouch(View v, MotionEvent event) {
-        	
-        	classCode.setInputType(InputType.TYPE_CLASS_TEXT);
-        	classCode.onTouchEvent(event); 
-        	
-            return true;
-            
-            } 
-            
+        Button myTimetable = (Button) findViewById(R.id.myTimetable);
+        Button searchTimetables = (Button) findViewById(R.id.searchTimetables);
+
+        myTimetable.setOnClickListener(new OnClickListener() {	
+            public void onClick(View v) {
+        	Intent myIntent = new Intent(v.getContext(), MyTimetable.class);
+                startActivityForResult(myIntent, 0);
+            }
         });
         
-        
-        btn.setOnClickListener(new OnClickListener() {
-        	
-            private EditText code = (EditText)findViewById(R.id.cdEntry);   
-            private EditText year = (EditText)findViewById(R.id.yrEntry);   
-            private RadioButton sem1 = (RadioButton)findViewById(R.id.sem1Btn);
-			
+        searchTimetables.setOnClickListener(new OnClickListener() {	
             public void onClick(View v) {
-        	
-        	if (code.length() == 0 || year.length() == 0) return;
-				
-        	String url = "http://www.dcu.ie/timetables/feed.php3" 
-        	    + "?hour=1-28&template=student";
-				
-        	// Add the class code to the URL.
-        	url += "&prog=" + code.getText();
-				
-        	// The year of study.
-        	url += "&per=" + year.getText();
-				
-        	if (sem1.isChecked()) {
-        	    
-        	    url += "&week1=1&week2=12";
-        	    
-        	} else {
-					
-        	    // Displayed on DCU website, these are the proper weeks.
-        	    url += "&week1=20&week2=31";
-        	    
-        	}
-				
-        	// Send out the intent to open the timetable in default browser.
-        	Intent i = new Intent(Intent.ACTION_VIEW);
-        	i.setData(Uri.parse(url));
-        	startActivity(i);
+        	Intent myIntent = new Intent(v.getContext(), SearchTimetable.class);
+                startActivityForResult(myIntent, 0);
             }
-            
         });
         
     }
