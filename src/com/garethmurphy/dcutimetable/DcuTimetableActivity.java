96fbc75b0b86da2e0fb3ae39aa98def5d0d8fa43
Colm Moore <mooreco4@gmail.com>
2011-12-08 01:26:59 +0000

    Added a chunk of code to stop the keyboard from automatically popping up when the app opens.

--- src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
+++ src/com/garethmurphy/dcutimetable/DcuTimetableActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,42 +17,70 @@ public class DcuTimetableActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         // Setup the event handler to process details and send the Intent.
-        Button btn = (Button)findViewById(R.id.button1);
+        final Button btn = (Button)findViewById(R.id.button1);
+        final EditText classCode = (EditText)findViewById(R.id.cdEntry);
+        
+       // This lump of code disables the keyboard from popping up on launch 
+        
+        classCode.setInputType(InputType.TYPE_NULL);
+    
+        classCode.setOnTouchListener(new View.OnTouchListener() {
+            
+            public boolean onTouch(View v, MotionEvent event) {
+        	
+        	classCode.setInputType(InputType.TYPE_CLASS_TEXT);
+        	classCode.onTouchEvent(event); 
+        	
+            return true;
+            
+            } 
+            
+        });
+        
+        
         btn.setOnClickListener(new OnClickListener() {
         	
-        	private EditText code = (EditText)findViewById(R.id.cdEntry);   
-			private EditText year = (EditText)findViewById(R.id.yrEntry);   
-			private RadioButton sem1 = (RadioButton)findViewById(R.id.sem1Btn);
+            private EditText code = (EditText)findViewById(R.id.cdEntry);   
+            private EditText year = (EditText)findViewById(R.id.yrEntry);   
+            private RadioButton sem1 = (RadioButton)findViewById(R.id.sem1Btn);
 			
-			public void onClick(View v) {
-				if (code.length() == 0 || year.length() == 0) return;
+            public void onClick(View v) {
+        	
+        	if (code.length() == 0 || year.length() == 0) return;
 				
-				String url = "http://www.dcu.ie/timetables/feed.php3" 
-						   + "?hour=1-28&template=student";
+        	String url = "http://www.dcu.ie/timetables/feed.php3" 
+        	    + "?hour=1-28&template=student";
 				
-				// Add the class code to the URL.
-				url += "&prog=" + code.getText();
+        	// Add the class code to the URL.
+        	url += "&prog=" + code.getText();
 				
-				// The year of study.
-				url += "&per=" + year.getText();
+        	// The year of study.
+        	url += "&per=" + year.getText();
 				
-				if (sem1.isChecked()) {
-					url += "&week1=1&week2=12";
-				} else {
+        	if (sem1.isChecked()) {
+        	    
+        	    url += "&week1=1&week2=12";
+        	    
+        	} else {
 					
-					// Displayed on DCU website, these are the proper weeks.
-					url += "&week1=20&week2=31";
-				}
+        	    // Displayed on DCU website, these are the proper weeks.
+        	    url += "&week1=20&week2=31";
+        	    
+        	}
 				
-				// Send out the intent to open the timetable in default browser.
-				Intent i = new Intent(Intent.ACTION_VIEW);
-				i.setData(Uri.parse(url));
-				startActivity(i);
-			}
-		});
+        	// Send out the intent to open the timetable in default browser.
+        	Intent i = new Intent(Intent.ACTION_VIEW);
+        	i.setData(Uri.parse(url));
+        	startActivity(i);
+            }
+            
+        });
+        
     }
+    
 }
